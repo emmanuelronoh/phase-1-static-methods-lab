aff92dcba055ad7595a68c7f1848f0a1c1ee6e0f
emmanuel ronoh <eronoh036@gmail.com>
2024-07-17 11:43:29 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// helpers.js
+
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^\w\s'-]/g, ''); 
+  }
+
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0 || !exceptions.includes(words[i])) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      }
+    }
+    return words.join(' ');
+  }
 }
+
+module.exports = Formatter;
